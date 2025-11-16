efa143f5a6ad757e8a6b099abb1e253d336a15d2
Diana Shkred <diana.private13@gmail.com>
2025-11-17 01:11:41 +0300

    add student

--- pr-1/task/index.js
+++ pr-1/task/index.js
@@ -11,6 +11,8 @@ studentService.loadFromFile();
 // Add demo students
 studentService.addStudent("John Doe", 20, 2);
 studentService.addStudent("Jane Smith", 23, 3);
+studentService.addStudent("Mike Johnson", 18, 2);
+
 
 // Display all
 logger.log("All students:", studentService.getAllStudents());
